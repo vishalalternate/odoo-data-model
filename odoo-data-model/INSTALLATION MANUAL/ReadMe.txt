7e632fe6b79e266a89eb2366be9a24f27fc5c6bb
vishalalternate <145339268+vishalalternate@users.noreply.github.com>
2023-09-29 15:43:00 +0530

    Add files via upload

---
+++ odoo-data-model/INSTALLATION MANUAL/ReadMe.txt
@@ -0,0 +1,17 @@
+
+------------------------------------------------------------------------------
+
+If you haven't bought the Odoo Data Model by Techneith yet
+
+visit -->https://techneith.com/products/odoo-data-model/
+
+where you do not have to pay for extra charges from any app store
+
+-------------------------------------------------------------------------------
+
+To see Installation guide of Odoo Data Model 
+
+visit --> https://techneith.com/resources/data-model-guide/
+
+
+-------------------------------------------------------------------------------
---
+++ odoo-data-model/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Data Model",
+
+    'summary': """
+       	 Odoo data model shows relations of a particular model with other models (Odoo Data Tables), click on the model node and voila! Nodes provide you with the nested relation of clicked models.""",
+
+    'description': """
+    Odoo data model shows relations of a particular model with other models (Odoo Data Tables), click on the model node and voila! Nodes provide you with the nested relation of clicked models. """,
+         'live_test_url': 'https://techneith.com/products/odoo-data-model/',
+    'maintainer': 'TN Canada',
+    'author': "TN Canada",
+    'website': "https://techneith.com/products/odoo-data-model/",
+    'support': "homerobotic21@gmail.com",
+ 
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '5.9',
+    'price': 0,
+    'currency': 'USD',
+    # any module necessary for this one to work correctly
+ 
+    'images': ['static/description/looker.gif'],
+    "license":"LGPL-3",
+
+    # always loaded
+   
+    # only loaded in demonstration mode
+    "application": True,
+    "installable": True,
+     'data': [
+
+    ],
+   'depends': [],
+}
